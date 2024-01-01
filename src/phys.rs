a21160349b16a1f468b50911e63774ac8cc9af13
HoneyPony <honeyponygames@gmail.com>
2024-01-01 17:19:46 -0500

    Not really working AABB

--- src/phys.rs
+++ src/phys.rs
@@ -19,8 +19,8 @@ pub struct PhysAABB {
 }
 
 impl PhysAABB {
-	pub fn top(&self) -> FixP { self.pos.y }
-	pub fn bottom(&self) -> FixP { FixP(self.pos.y.0 + self.size.y.0) }
+	pub fn bottom(&self) -> FixP { self.pos.y }
+	pub fn top(&self) -> FixP { FixP(self.pos.y.0 + self.size.y.0) }
 
 	pub fn left(&self) -> FixP { self.pos.x }
 	pub fn right(&self) -> FixP { FixP(self.pos.x.0 + self.size.x.0) }
@@ -65,71 +65,95 @@ pub fn aabb_tiles(x: i32, y: i32, width: i32, height: i32) -> PhysAABB {
 	return aabb_subpx(x * mul, y * mul, width * mul, height * mul)
 }
 
-fn y_dist(r1: &PhysAABB, r2: &PhysAABB) -> FixP {
+fn y_dist(r1: &PhysAABB, r2: &PhysAABB) -> Option<FixP> {
 	if r1.top().0 < r2.bottom().0 {
-		return FixP(- (r2.bottom().0 - r1.top().0))
+		return Some(FixP( (r2.bottom().0 - r1.top().0)))
 	}
 
 	if r2.top().0 < r1.bottom().0 {
-		return FixP(r1.bottom().0 - r2.top().0)
+		return Some(FixP(- (r1.bottom().0 - r2.top().0)))
 	}
 
 	// Overlapping -- TODO figure out what we want to do
-	return FixP(0);
+	None
 }
 
-fn x_dist(r1: &PhysAABB, r2: &PhysAABB) -> FixP {
+fn x_dist(r1: &PhysAABB, r2: &PhysAABB) -> Option<FixP> {
 	if r1.right().0 < r2.left().0 {
-		return FixP(- (r2.left().0 - r1.right().0))
+		return Some(FixP( (r2.left().0 - r1.right().0)))
 	}
 
 	if r2.right().0 < r1.left().0 {
-		return FixP(r1.left().0 - r2.right().0)
+		return Some(FixP(- (r1.left().0 - r2.right().0)))
 	}
 
-	return FixP(0)
+	None
 }
 
 pub fn move_and_slide(aabb: &mut PhysAABB, own_id: Entity, velocity: PhysVec, world: &mut World) -> PhysVec {
 	// For each AABB in the world that isn't our own_id, we will clamp the velocity.
 	let mut vx = velocity.x.0;
 	let mut vy = velocity.y.0;
+
+	println!("incoming vx vy = {} {}", vx, vy);
 	
 	for (other, id) in world.query::<(&PhysAABB, Entity)>().iter(&world) {
 		if id == own_id { continue; }
 
-		let x = x_dist(aabb, other).0;
-		if i32::signum(x) == i32::signum(vx) {
-			// If we're moving in this direction, clamp x if necessary.
-			if i32::abs(vx) > i32::abs(x) {
-				// If vx is larger than x, we need to scale the y velocity
-				// by the amount that we're scaling vx by.
-				if x != 0 {
-					vy = (vy * vx) / (x * 256); // Fixed point multiply
-				}
+		if let Some(x) = x_dist(aabb, other) {
+			let x = x.0;
+			println!("x dist = {}", x);
+			if i32::signum(x) == i32::signum(vx) {
+				// If we're moving in this direction, clamp x if necessary.
+				if i32::abs(vx) > i32::abs(x) - 1 {
+					// If vx is larger than x, we need to scale the y velocity
+					// by the amount that we're scaling vx by.
+					if x != 0 {
+						vy = (vy * vx) / (x * 256); // Fixed point multiply
+					}
 
-				vx = i32::signum(vx) * i32::abs(x);
+					vx = i32::signum(vx) * (i32::abs(x) - 1);
+				}
 			}
+			//else if x == 0 {
+			//	vx = 0;
+			//}
 		}
 
-		let y = y_dist(aabb, other).0;
-		if i32::signum(y) == i32::signum(vy) {
-			// If we're moving in this direction, clamp x if necessary.
-			if i32::abs(vy) > i32::abs(y) {
-				// If vx is larger than x, we need to scale the y velocity
-				// by the amount that we're scaling vx by.
-				if y != 0 {
-					vx = (vy * vx) / (y * 256); // Fixed point multiply
-				}
+		if let Some(y) = y_dist(aabb, other) {
+			let y = y.0;
+			println!("y dist = {}", y);
+			if i32::signum(y) == i32::signum(vy) {
+				// If we're moving in this direction, clamp x if necessary.
+				if i32::abs(vy) > i32::abs(y) - 1 {
+					//println!("should do a collide -> {} vs {}", vy, y);
+					// If vx is larger than x, we need to scale the y velocity
+					// by the amount that we're scaling vx by.
+					if y != 0 {
+						vx = (vy * vx) / (y * 256); // Fixed point multiply
+					}
 
-				vy = i32::signum(vy) * i32::abs(y);
+					vy = i32::signum(vy) * (i32::abs(y) - 1);
+					println!("new vy = {}", vy);
+				}
 			}
+			//else if y == 0 {
+			//	vy = 0;
+			//}
 		}
+
+		//println!("x dist: {} y dist: {}", x, y);
 	}
 
 	// Now apply the velocity to the AABB.
 	aabb.pos.x.0 += vx;
 	aabb.pos.y.0 += vy;
+
+	for (other, id) in world.query::<(&PhysAABB, Entity)>().iter(&world) {
+		if id == own_id { continue; }
+
+		println!("after moving -- x = {:?}, y = {:?}", x_dist(aabb, other).map(|a| a.0), y_dist(aabb, other).map(|a| a.0));
+	}
 
 	zero()
 }
